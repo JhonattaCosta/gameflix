503275800568dbc7b1d14d3fab0a5fe2a55c67c7
JhonattaCosta <jhonattasantos27@gmail.com>
2025-07-10 21:25:46 -0300

    feat: Add saveGame method to save new games
    - Implement the saveGame method in GameService

--- src/main/java/com/gameflix/service/GameService.java
+++ src/main/java/com/gameflix/service/GameService.java
@@ -13,6 +13,10 @@ public class GameService {
 
     private final GameRepostiory repostiory;
 
+    public Game saveGame(Game game){
+        return repostiory.save(game);
+    }
+
     public List<Game> findAllGames(){
         return repostiory.findAll();
     }
