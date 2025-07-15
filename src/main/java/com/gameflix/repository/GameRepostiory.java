9a7e387f03ba3936f71f428dc4714099a2fdcf56
JhonattaCosta <jhonattasantos27@gmail.com>
2025-07-14 22:15:47 -0300

    feat: add method findGameByCategories List

--- src/main/java/com/gameflix/repository/GameRepostiory.java
+++ src/main/java/com/gameflix/repository/GameRepostiory.java
@@ -1,7 +1,13 @@
 package com.gameflix.repository;
 
-import com.gameflix.entity.Game;
+import com.gameflix.model.Category;
+import com.gameflix.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GameRepostiory extends JpaRepository<Game, Long> {
+
+    List<Game> findGamebyCategories(List<Category> categories);
+
 }
