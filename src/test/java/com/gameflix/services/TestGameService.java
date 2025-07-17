a7bd465fd6403bc107cc88d0b222f15d9453fc7e
JhonattaCosta <jhonattasantos27@gmail.com>
2025-07-17 18:40:39 -0300

    test: add tests for find all games, find game by id, find games by category with flexible list comparison, update game and delete game
     - Created test for 'findByCategory' with flexible list comparison using 'anyList()' to handle argument matching.

--- src/test/java/com/gameflix/services/TestGameService.java
+++ src/test/java/com/gameflix/services/TestGameService.java
@@ -2,7 +2,9 @@ package com.gameflix.services;
 
 import com.gameflix.model.Category;
 import com.gameflix.model.Game;
+import com.gameflix.repository.CategoryRepository;
 import com.gameflix.repository.GameRepostiory;
+import com.gameflix.service.CategoryService;
 import com.gameflix.service.GameService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +27,11 @@ public class TestGameService {
 
     @Mock
     private GameRepostiory repository;
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private CategoryService categoryService;
 
     @InjectMocks
     private GameService service;
@@ -75,6 +83,7 @@ public class TestGameService {
         List<Game> gamesfake = service.findAllGames();
 
         assertEquals(2,gamesfake.size());
+        assertEquals(games,gamesfake);
     }
 
     @Test
@@ -93,8 +102,84 @@ public class TestGameService {
         Optional<Game> findIdGame = service.findGameById(1L);
 
         assertTrue(findIdGame.isPresent());
-        assertEquals(findIdGame, gamefake);
+        assertEquals(findIdGame.get(), gamefake.get());
     }
 
+    @Test
+    public void testFindGameByCategory(){
+        List<Category> expectedCategories = Arrays.asList(
+                new Category(1L, "Ação")
+        );
+        List<Game> games = Arrays.asList(
+                new Game(
+                        1L,
+                        "GTA 6",
+                        "Jogo de mundo aberto",
+                        4,
+                        LocalDate.of(2026, 06, 26),
+                        categories
+                ),
+                new Game(
+                        1L,
+                        "PUBG",
+                        "BattleGround",
+                        4,
+                        LocalDate.of(2026, 06, 26),
+                        categories
+                ));
+        when(repository.findGamebyCategories(anyList())).thenReturn(games);
 
+        List<Game> gamesFind = service.findByCategory(1L);
+
+       assertEquals(2, gamesFind.size());
+       assertTrue(gamesFind.containsAll(games));
+    }
+
+    @Test
+    public void testUpdateGames(){
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1L, "Ação"));
+        categories.add(new Category(2L, "Terceira-Pessoa"));
+        Game gameFake = new Game(
+                1L,
+                "GTA 61",
+                "Jogo de mundo aberto",
+                4,
+                LocalDate.of(2026, 06, 26),
+                categories
+        );
+        Game gameUpdate = new Game(
+                1L,
+                "GTA 6",
+                "Jogo de mundo aberto",
+                4,
+                LocalDate.of(2026, 06, 26),
+                categories
+        );
+
+
+        when(repository.findById(1L)).thenReturn(Optional.of(gameFake));
+        when(repository.save(gameFake)).thenReturn(gameFake);
+
+        Optional<Game> result = service.updateGame(1L, gameUpdate);
+
+        assertTrue(result.isPresent());
+        assertEquals("GTA 6", result.get().getName());
+
+
+    }
+
+    @Test
+    public void testDeleteGame(){
+        Game gamefake = new Game(
+                1L,
+                "GTA 6",
+                "Jogo de mundo aberto",
+                4,
+                LocalDate.of(2026, 06, 26),
+                categories
+        );
+        service.deleteById(1L);
+        verify(repository).deleteById(1L);
+    }
 }
