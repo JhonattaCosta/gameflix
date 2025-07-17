53783516a52230b6137f21983f99150e2c719eeb
JhonattaCosta <jhonattasantos27@gmail.com>
2025-07-17 18:04:45 -0300

    feat: add update, delete, and find game by category services; instantiate CategoryService in GameService

--- src/main/java/com/gameflix/service/GameService.java
+++ src/main/java/com/gameflix/service/GameService.java
@@ -6,6 +6,7 @@ import com.gameflix.repository.GameRepostiory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ import java.util.Optional;
 public class GameService {
 
     private final GameRepostiory repostiory;
+    private final CategoryService categoryService;
 
     public Game saveGame(Game game){
         return repostiory.save(game);
@@ -27,8 +29,39 @@ public class GameService {
         return repostiory.findById(id);
     }
 
+    public List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category ->
+                categoryService.findCategoryById(category.getId()).ifPresent(categoriesFound::add)
+        );
+        return categoriesFound;
+    }
+
+    public Optional<Game> updateGame(Long gameId, Game updateGame){
+        Optional<Game> optGame = repostiory.findById(gameId);
+        if (optGame.isPresent()){
+            List<Category> categories = this.findCategories(updateGame.getCategories());
+
+            Game game = optGame.get();
+            game.setName(updateGame.getName());
+            game.setDescription(updateGame.getDescription());
+            game.setRating(updateGame.getRating());
+            game.setDateRelease(updateGame.getDateRelease());
+
+            game.getCategories().clear();
+            game.getCategories().addAll(categories);
+
+            repostiory.save(game);
+            return Optional.of(game);
+        }
+        return Optional.empty();
+    }
+
     public List<Game> findByCategory(Long categoryId){
         return repostiory.findGamebyCategories(List.of(Category.builder().id(categoryId).build()));
     }
 
+    public void deleteById(Long id){
+        repostiory.deleteById(id);
+    }
 }
