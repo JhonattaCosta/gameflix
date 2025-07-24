6626fcc202b7d51a8060015deb4230d831654cd8
JhonattaCosta <jhonattasantos27@gmail.com>
2025-07-23 21:23:01 -0300

    refactor: update GameRepository to search games by categoryId
    - Updated query to fetch games by category ID
    
    - Adjusted GameService to use the new repository method and return DTOs

--- src/main/java/com/gameflix/repository/GameRepostiory.java
+++ src/main/java/com/gameflix/repository/GameRepostiory.java
@@ -1,13 +1,15 @@
 package com.gameflix.repository;
 
-import com.gameflix.model.Category;
 import com.gameflix.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GameRepostiory extends JpaRepository<Game, Long> {
 
-    List<Game> findByCategoriesIn(List<Category> categories);
+    @Query("SELECT g FROM Game g JOIN g.categories c WHERE c.id = :categoryId")
+    List<Game> findByCategoryId(@Param("categoryId") Long categoryId);
 
 }
